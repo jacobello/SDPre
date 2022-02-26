7fd688f9046ce6964ee53351bb8b5cc2e8ed46f2
Jacob Nguyen <jn@jacobello.com>
2022-02-25 17:36:15 -0700

    finish O3L2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args){
+double side1, side2, hypotenuse;
+side1 = 8;
+side2 = 10;
+double result = (side1 * side1) + (side2 * side2);
+hypotenuse = Math.sqrt(result);
+System.out.println(hypotenuse);
+}
+}
