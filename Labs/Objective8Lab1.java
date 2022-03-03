2e79af696a56ca8cda80b698b9acd9c3b556d7ee
Jacob Nguyen <jn@jacobello.com>
2022-03-03 11:59:57 -0700

    Finish Ob8L1 FOR

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1{
+  public static void main(String[]args){
+    int n;
+    for(n=1; n<=10; n+=1){
+      System.out.println(n);
+    }
+    for(n=10; n>=1; n-=1){
+      System.out.println(n);
+    }
+  }
+}
