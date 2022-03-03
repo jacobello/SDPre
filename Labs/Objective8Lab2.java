44216ca348ce6ecc58ced9c84cdba78d4d39ca8e
Jacob Nguyen <jn@jacobello.com>
2022-03-03 12:25:20 -0700

    FinishO8L2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2{
+  public static void main(String[]args){
+    int n =0 ;
+    int sum = n;
+
+  for(n=0; n<=20; n+=1){
+      sum = sum + n;
+
+  }
+  System.out.println(sum);
+
+  }
+}
