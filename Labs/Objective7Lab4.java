589169d998ce56718902f9a6ef1da44972226e95
Jacob Nguyen <jn@jacobello.com>
2022-03-03 11:13:38 -0700

    Finish O7L4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+  public static void main(String[]args){
+int count = 0;
+int sum = count;
+while (count <=19){
+  count = count + 1;
+  sum = sum + count;
+
+System.out.println(sum);
+    }
+  }
+}
